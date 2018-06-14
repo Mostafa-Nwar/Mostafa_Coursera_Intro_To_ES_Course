f233ea822b4fe35ef166692def0f55a3c5fb4433
Mostafa Samir Nwar <mostafa.nwar@yahoo.com>
2018-06-14 00:30:03 -0700

    Third-Commit Implementing all functions
    Program Finished

--- stats.c
+++ stats.c
@@ -40,32 +40,93 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+	sort_array(test,SIZE);
+	print_array(test,SIZE);
+	print_statistics(test,SIZE);
 
 }
-
-/* Add other Implementation File Code Here */
-
+//**********************************************************
 void print_statistics(unsigned char * arr , int size ){
-	
+	printf("******* The previous Showed Array are processed\nAnd produced these Statistics \n ");
+	printf("******* The Value of median is  :%d \n",find_median (arr,size));
+	printf("******* The Value of mean is    :%d \n",find_mean   (arr,size));
+	printf("******* The Value of maximum is :%d \n",find_maximum(arr,size));
+	printf("******* The Value of minimum is :%d \n",find_minimum(arr,size));
+	printf("*************************************\n");
 }
 //**********************************************************
 void print_array(unsigned char * arr , int size ){
+	unsigned char i ; 
+	printf("The Sorted array is as follow :\n");
+	for(i=0;i<size;++i){
+	printf("Elemet Number %d  is : %d \n",i+1,arr[i]);
+	}
+	printf("*************************************\n");
 }
 //***********************************************************
 int find_median(unsigned char * arr, int size){
+/*as requirment for a median the array must be sorted so */
+sort_array(arr,size);
+/*check if the size even or anything else */
+if((size%2)==0){
+	
+	return((int)((arr[size/2]+arr[(size/2)+1])/2)) ;
+}
+else {
+	return(arr[(int)(size/2)]);
+}
+
+
 }
 //**************************************************************
 int find_mean(unsigned char * arr, int size ){
-
+ unsigned char i ; 
+ unsigned int sum=0 ;
+	for(i=0;i<size;++i){
+	sum= sum+arr[i];
+	}
+return ((int)(sum/size));
 }
 //***************************************************************
 int find_maximum(unsigned char * arr , int size){
+ unsigned char i ;
+ unsigned char max=0 ; 
+	for(i=0;i<size;++i){
+		if(max < arr[i]){
+		max=arr[i];
+		}
+		else {/*Do nothing*/}
+	}
+	return max; 
 
 }
 //******************************************************************
 int find_minimum(unsigned char * arr , int size ){
+ unsigned char i ;
+ unsigned char min=255 ; 
+	for(i=0;i<size;++i){
+		if(min > arr[i]){
+		min=arr[i];
+		}
+		else {/*Do nothing*/}
+	}
+	return min; 
 }
 //**************************************************************
 void sort_array(unsigned char * arr , int size ){
+	unsigned char i,j ; 
+	int temp ; 
+for(j=1;j<SIZE;++j){
+	for(i=0;i<size-j;++i){
+		if(arr[i]<arr[i+1]){
+		
+			temp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1]=temp; 
+		
+		}
+		else {/*Do nothing*/ }
+	}
+}
 }
 
